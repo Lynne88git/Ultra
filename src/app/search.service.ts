10fb20de5998f7b1a9e2eb998e0f97ef501e6a73
geldenhuys.lynne@gmail.com <geldenhuys.lynne@gmail.com>
2021-11-09 18:27:26 +0200

    Adding Search Component, Search Service, Routing

---
+++ src/app/search.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of, empty } from 'rxjs';
+import { map } from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+
+  constructor() { }
+
+
+}
