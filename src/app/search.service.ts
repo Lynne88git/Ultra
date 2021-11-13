57999f6c4035a00182e3aac520360032e30cafda
geldenhuys.lynne@gmail.com <geldenhuys.lynne@gmail.com>
2021-11-13 17:23:27 +0200

    Basic search functionality added with paging function, tbc

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -8,7 +8,32 @@ import { map } from "rxjs/operators";
 })
 export class SearchService {
 
-  constructor() { }
+  public baseUrl = "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC";
+  public searchResults: any;
 
+  constructor(private httpClient: HttpClient) { }
+  
+  //makes Http call to the API
 
+  public searchEntries(term: any): Observable<any>{
+    if(term === ""){
+      console.log("Not Defined");
+      return of(null);
+      //return empty()
+    }
+    else {
+      let params = {q: term}
+
+      return this.httpClient.get(this.baseUrl, {params}).pipe(
+        map(response => {
+          console.log(response);
+          return this.searchResults = { response:["items"] };
+        })
+      );
+    }
+  }
+//returns the response
+public _searchEntries(term: any){
+  return this.searchEntries(term);
+}
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { Subject, throwError } from "rxjs";
+import { map, debounceTime, distinctUntilChanged, switchMap, catchError, retry } from "rxjs/operators";
+import { SearchService } from '../search.service';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +11,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(private searchService: SearchService) { }
 
-  ngOnInit(): void {
+  public loading: boolean;
+  public searchTerm = new Subject<string>();
+  public searchResults: any;
+  public paginationElements: any; 
+  public errorMessage: any;
+  public page: any;
+
+  public searchForm = new FormGroup({
+    search: new FormControl("", Validators.required),
+  });
+
+  public search(){
+    this.searchTerm.pipe(
+      map((e: any) =>{
+        console.log(e.target.value);
+        return e.target.value
+      }),
+      debounceTime(400),
+      distinctUntilChanged(),
+      switchMap(term => {
+        this.loading = true;
+        return this.searchService._searchEntries(term);
+      }),
+      catchError((e) => {
+        console.log(e);
+        this.loading = false;
+        this.errorMessage = e.message;
+        return throwError(e);
+      }),
+    ).subscribe( v => {
+      this.loading = true;
+      this.searchResults = v;
+      this.paginationElements = this.searchResults;
+    })
+  }
+
+  ngOnInit() {
+    this.search();
   }
 
 }
